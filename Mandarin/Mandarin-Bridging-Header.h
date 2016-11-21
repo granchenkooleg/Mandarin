ae210110a5324cfa152d8497da0aa1762d0ba04d
Yura Granchenko <yura-granchenko@yandex.ru>
2016-11-21 21:04:32 +0200

    add facebook framework

---
+++ Mandarin/Mandarin-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Mandarin-Bridging-Header.h
+//  Mandarin
+//
+//  Created by Macostik on 11/21/16.
+//  Copyright © 2016 Oleg. All rights reserved.
+//
+
+#ifndef Mandarin_Bridging_Header_h
+#define Mandarin_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+
+#endif /* Mandarin_Bridging_Header_h */
