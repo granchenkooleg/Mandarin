a4c1fe8d1a84c6962562f5cd48b7688b1ce6d0e0
Oleg <granchenkooleg@gmail.com>
2017-01-11 01:38:55 +0200

    add actions in PaymentVC

--- Mandarin/Mandarin/PaymentViewController.swift
+++ Mandarin/Mandarin/PaymentViewController.swift
@@ -10,33 +10,79 @@ import UIKit
 
 class PaymentViewController: BasketViewController {
     
+    @IBOutlet weak var needChangeButton: UIButton!
+    @IBOutlet weak var noButton: UIButton!
+    @IBOutlet weak var continueButton: UIButton!
     @IBOutlet weak var totalPriceForPaymentVCLabel: UILabel!
-
-     override func viewDidLoad() {
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         totalPriceForPaymentVCLabel?.text = (totalPriceInCart() + " грн.,")
+        
+        //Set continueButton hidden at start
+        continueButton.isHidden = true
     }
-
-     override func didReceiveMemoryWarning() {
+    
+    override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func needChangeButton(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Сдача", message: "Пожалуйста, впишите вашу купюру", preferredStyle: .alert)
+        
+        let actionCancel = UIAlertAction(title: "Отменить", style: .cancel) { (action:UIAlertAction) in
+            //This is called when the user presses the cancel button.
+            print("You've pressed the cancel button");
+        }
+        
+        let actionOk = UIAlertAction(title: "Ок", style: .default) { (action:UIAlertAction) in
+            //This is called when the user presses the login button.
+            let textUser = alertController.textFields![0] as UITextField
+            
+            
+            print("The user entered:%@ ",textUser.text!);
+        }
+        
+        alertController.addTextField { (textField) -> Void in
+            //Configure the attributes of the first text box.
+            textField.placeholder = "123"
+        }
+        
+        //Add the buttons
+        alertController.addAction(actionCancel)
+        alertController.addAction(actionOk)
+        
+        //Present the alert controller
+        self.present(alertController, animated: true, completion:nil)
+        
+        continueButton.isHidden = false
+        noButton.isHidden = true
+        needChangeButton.isHidden = true
+    }
+    
+    //Here is the action when you press noButton which is visible
+    @IBAction func noButton(_ sender: UIButton) {
+        continueButton.isHidden = false
+        noButton.isHidden = true
     }
     
     //MARK: Sender to CheckVC
     @IBAction func CheckClick(_ sender: UIButton) {
         present(UIStoryboard.main["checkVC"]!, animated: true, completion: nil)
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
