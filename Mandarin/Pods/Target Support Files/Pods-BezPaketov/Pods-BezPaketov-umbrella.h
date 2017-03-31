6d5e504ad268cc67c8706e8205aa995e7c05930f
Macostik <yura-granchenko@yandex.ru>
2017-03-31 22:07:17 +0300

    regenerate xcodeworkspace

--- Mandarin/Pods/Target Support Files/Pods-BezPaketov/Pods-BezPaketov-umbrella.h
+++ Mandarin/Pods/Target Support Files/Pods-BezPaketov/Pods-BezPaketov-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_BezpaketovVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_BezpaketovVersionString[];
+FOUNDATION_EXPORT double Pods_BezPaketovVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BezPaketovVersionString[];
 
